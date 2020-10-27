cd253aa65959417e56a59ba2401bed7471c3da5c
David Pan <dpan5425@gmail.com>
2020-10-27 10:57:27 -0400

    solved

--- src/App.js
+++ src/App.js
@@ -10,9 +10,15 @@ function App() {
     <Wrapper>
       <Title>Friends List</Title>
 
-      <FriendCard name={friends[0].name} image={friends[0].image} occupation={friends[0].occupation} location={friends[0].location} />
+      {friends.map(friend => {
+        return <FriendCard name={friend.name} image={friend.image} occupation={friend.occupation} location={friend.location} />
+      })}
+
+
+
+      {/* <FriendCard name={friends[0].name} image={friends[0].image} occupation={friends[0].occupation} location={friends[0].location} />
       <FriendCard name={friends[1].name} image={friends[1].image} occupation={friends[1].occupation} location={friends[1].location} />
-      <FriendCard name={friends[2].name} image={friends[2].image} occupation={friends[2].occupation} location={friends[2].location} />
+      <FriendCard name={friends[2].name} image={friends[2].image} occupation={friends[2].occupation} location={friends[2].location} /> */}
       
     </Wrapper>
   );
