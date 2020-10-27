90e21fced8f14accfa6cfcb2ae83cbbd661d0f5e
David Pan <dpan5425@gmail.com>
2020-10-27 11:26:41 -0400

    solved

--- src/components/List.js
+++ src/components/List.js
@@ -3,8 +3,13 @@ import React from "react";
 function List(props) {
   return (
     <ul className="list-group">
-      Using the map method, render one li element displaying the text property of each grocery
-      object.
+      {/* {props.groceries.map(grocery => (
+        <li key={grocery.id} className="list-group-item">{grocery.name}</li>
+      ))} */}
+ 
+      {props.groceries.filter(grocery => grocery.purchased).map(grocery => (
+        <li key={grocery.id} className="list-group-item">{grocery.name}</li>
+      ))}
     </ul>
   );
 }
